03243fa834102143e9b20d4484f9422436baf1fb
akiratakeda <takeda1411123@gmail.com>
2024-04-11 17:08:40 +0100

    fix

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,7 +2,7 @@
 export const headerData = {
     name: 'Akira Takeda',
     title: "Hello! I am",
-    desciption: "Hello! I am Akira, I am Game, Software, and Data Engineer",
+    desciption: "Hello! I'm Akira, I am Game, Software, and Data Engineer",
     image: 'https://public-portfolio-at.s3.amazonaws.com/IMG_5166.JPG',
     imagebw: 'https://public-portfolio-at.s3.amazonaws.com/IMG_5166.JPG',
     resumePdf: 'https://docs.google.com/document/d/xxxx'
